fe17ef9e231d4142caed233fe757c30dd26b645b
Ashiock <00039619@uca.edu.sv>
2021-05-25 18:23:14 -0600

    Add bolipret Screen

---
+++ app/src/main/java/com/example/estudioprogra/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.estudioprogra.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.estudioprogra.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
